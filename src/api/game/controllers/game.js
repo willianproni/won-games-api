8b508bbb813eec9f6f08cfca47f1a09909c9694b
willianproni <wifepro@hotmail.com>
2023-03-11 14:47:49 -0300

    Criando a servises e fazendo get na Api do gog (site de jogos) para pegar os dados

--- src/api/game/controllers/game.js
+++ src/api/game/controllers/game.js
@@ -9,7 +9,13 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::game.game', ({strapi}) => ({
     async populate(ctx) {
         try {
-          ctx.body = 'ok';
+          console.log("Start to populate...")
+          
+          await strapi.service('api::game.game').populate()
+
+          console.log(ctx.query)
+
+          ctx.send("Finish Populating!");
         } catch (err) {
           ctx.body = err;
         }
--- src/api/game/services/game.js
+++ src/api/game/services/game.js
@@ -3,7 +3,18 @@
 /**
  * game service
  */
-
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::game.game');
+const axios = require("axios")
+
+module.exports = createCoreService('api::game.game', ({ strapi }) => ({
+
+    async populate(params) {
+        const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3Atrending&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`
+
+        const { data: { products } } = await axios.get(gogApiUrl)
+
+        console.log(products[0])
+    }
+
+}));
